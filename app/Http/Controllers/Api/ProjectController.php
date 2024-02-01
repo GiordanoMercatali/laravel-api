72f45f2629bd00abebd01ce3d5ab3473812c883d
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2024-02-01 18:10:05 +0100

    changed: query from project with type to all

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -10,7 +10,8 @@ class ProjectController extends Controller {
 
     public function index(){
         
-        $projects = Project::with('type');
+        // $projects = Project::with('type');
+        $projects = Project::all();
 
         return response()->json([
             'result' => $projects,
