206841f1752988a877cf57861a9562113a34b951
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2024-02-01 16:28:06 +0100

    add:routes to api.php

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\TypeController;
+use App\Http\Controllers\Admin\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('/projects', [ProjectController::class, 'index']);
+Route::get('/types', [TypeController::class, 'index']);
